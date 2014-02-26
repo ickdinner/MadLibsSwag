6e41d888548da93b597380dd430a51c21a380716
ickdinner <ethan.oliver@gmail.com>
2014-02-26 08:56:12 -0500

    Fixed Start Button

--- src/com/example/madlibsswag/ChooseActivity.java
+++ src/com/example/madlibsswag/ChooseActivity.java
@@ -1,9 +1,17 @@
 package com.example.madlibsswag;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class ChooseActivity {
 
-	public void onCreate (Bundle savedInstanceState) {
+public class ChooseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.choose_activity);
 	}
+
+
 }
--- src/com/example/madlibsswag/MainActivity.java
+++ src/com/example/madlibsswag/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Button myButton = (Button)this.findViewById(R.id.startgame);
+		//swag
 	}
 	
 	
